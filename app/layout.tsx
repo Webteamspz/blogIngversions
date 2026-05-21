d3d062c86662c644cca94357c562f337c70badf3
Writwik-work <Writwik.work@gmail.com>
2026-05-21 12:33:46 +0530

    Gtm Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
 import { Barlow } from "next/font/google";
+import Script from "next/script";
+
 import "./globals.css";
 
-// 1. Sahi Imports (Check karo ki PreloaderWrapper sahi jagah se import ho)
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Preloader from "./components/Preloader/Preloader"; 
+import Preloader from "./components/Preloader/Preloader";
 
-const barlow = Barlow({ 
+import GTMProvider from "./GTMProvider";
+
+const barlow = Barlow({
   subsets: ["latin"],
-  weight: ["400", "600", "700", "800"] 
+  weight: ["400", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -22,19 +25,65 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body className={`${barlow.className} flex flex-col min-h-screen`}>
-        
-        {/* 2. Yahan PreloaderWrapper use karna hai, Preloader nahi! */}
+
+      <head>
+
+        {/* Google Tag Manager Script */}
+        <Script
+          src={`https://www.googletagmanager.com/gtm.js?id=GTM-P5BGX7H7`}
+          strategy="afterInteractive"
+        />
+
+        {/* GTM Init */}
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            window.dataLayer.push({
+              'gtm.start': new Date().getTime(),
+              event: 'gtm.js'
+            });
+          `}
+        </Script>
+
+      </head>
+
+      <body
+        className={`${barlow.className} flex flex-col min-h-screen`}
+      >
+
+        {/* GTM Provider */}
+        <GTMProvider />
+
+        {/* GTM NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5BGX7H7"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
         <Preloader>
+
           <Header />
-          
+
           <main className="grow">
             {children}
           </main>
-          
+
           <Footer />
+
         </Preloader>
 
       </body>
